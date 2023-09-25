8dc29d205c206ed60c4138f1aa3cbdf4467ef576
nullptr <nullptr@localhost>
2023-09-25 15:34:55 +0800

    chore: support at server (wip)

--- examples/at_server_demo/app.mk
+++ examples/at_server_demo/app.mk
@@ -24,7 +24,6 @@ SCENARIO_APP_INCDIR += $(SCENARIO_APP_SUPPORT_SORTED)
 
 SCENARIO_APP_INCDIR += $(SSCMA_INCDIRS)
 
-
 SCENARIO_APP_CSRCS = $(call get_csrcs, $(SCENARIO_APP_INCDIR))
 SCENARIO_APP_CXXSRCS = $(call get_cxxsrcs, $(SCENARIO_APP_INCDIR))
 SCENARIO_APP_CCSRCS = $(call get_ccsrcs, $(SCENARIO_APP_INCDIR))
@@ -33,9 +32,8 @@ SCENARIO_APP_ASMSRCS = $(call get_asmsrcs, $(SCENARIO_APP_ASMSRCDIR))
 SCENARIO_APP_CSRCS += $(SSCMA_CSRCS)
 SCENARIO_APP_CXXSRCS += $(SSCMA_CXXSRCS)
 
-# ALGO_CONFIG_FILE := $(shell realpath ./examples/${APP}/app.conf)
-
-# ifneq ($(wildcard $(ALGO_CONFIG_FILE)),)
-# LINES := $(shell cat $(ALGO_CONFIG_FILE))
-# APPL_DEFINES += $(foreach line,$(LINES),-D$(line))
-# endif
+ALGO_CONFIG_FILE := $(shell realpath ./examples/${APP}/app.conf)
+ifneq ($(wildcard $(ALGO_CONFIG_FILE)),)
+LINES := $(shell cat $(ALGO_CONFIG_FILE))
+APPL_DEFINES += $(foreach line,$(LINES),-D$(line))
+endif
